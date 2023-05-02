8bde5273bb10437477dfb5c588af781564422caa
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-02 12:24:28 +0800

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Dmitrii Melnikov on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
